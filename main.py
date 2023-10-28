2644d5df17f39b624f3a8f5dd14b47e23db88bf4
Shadow <agent.777@inbox.ru>
2023-10-28 15:47:44 +0300

    ДЗ5: Напишите match, который принимает на вход строки, и выводит на экран сообщения «сохранить» для строки «save», «загрузить» для строки «load», и «неизвестная операция» всех остальных строк.

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-global_value = 0
-
-def SumScopes(param):
-    ext_val = int(input("Введите значение переменной из охватывающей области: "))
-
-    def inner_func(param):
-        local_val = int(input("Введите значение локальной переменной: "))
-        global global_value
-        return global_value + ext_val + param + local_val
-
-    return inner_func(param)
-
-global_value = int(input("Введите значение глобальной переменной: "))
-print( SumScopes(int(input("Введите значение параметра, передаваемого в функцию"))) )
+msg = input("Введите команду (save, load, something else): ")
+match msg:
+    case 'save':
+        print ("Сохранить")
+    case 'load':
+        print ("Загрузить")
+    case _:
+        print ("Неизвестная операция")
