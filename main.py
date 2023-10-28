d8c83224bfec44ca72a2ad743c30d0a6934f0e4a
Shadow <agent.777@inbox.ru>
2023-10-28 16:31:32 +0300

    ДЗ6: Напишите программу, считающую, сколько раз в строке встречается слово «кот», написанное либо целиком заглавными буквами (КОТ), либо целиком прописными (кот), либо с заглавной буквы (Кот), но не учитывая прочие варианты (кОТ и т. д.).

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-msg = input("Введите команду (save, load, something else): ")
-match msg:
-    case 'save':
-        print ("Сохранить")
-    case 'load':
-        print ("Загрузить")
-    case _:
-        print ("Неизвестная операция")
+input_string = input("Введите строку: ")
+words = input_string.split()
+count = 0
+for word in words:
+    match word:
+        case "кот" | "КОТ" | "Кот":
+            count += 1
+        case _:
+            continue
+print(count)
