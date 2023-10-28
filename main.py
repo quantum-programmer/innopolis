0432f4bc39dfcdd2ab18cd22dc791a598f8d362b
Shadow <agent.777@inbox.ru>
2023-10-28 16:51:49 +0300

    ДЗ7: Напишите программу, которая превратит список списков в одноуровневый список, где все элементы предыдущего списка идут в том же порядке.
    (Вариант 1)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-input_string = input("Введите строку: ")
-words = input_string.split()
-count = 0
-for word in words:
-    match word:
-        case "кот" | "КОТ" | "Кот":
-            count += 1
-        case _:
-            continue
-print(count)
+people = [
+    ["Alex", 29],
+    ["Olga", 33],
+    ["Vadim", 27],
+    ['Nadya', 36],
+    ['Petr', 40]
+]
+
+people_1_level = list()
+for person in people:
+    for elem in person:
+        people_1_level.append(elem)
+
+print (f"Исходный список списков: {people}")
+print (f"Одноуровневый список: {people_1_level}")
